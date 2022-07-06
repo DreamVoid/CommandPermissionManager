489f4872044fcc2c2e58cbe221dd26d905ea8785
DreamVoid <45266046+DreamVoid@users.noreply.github.com>
2022-07-07 02:27:55 +0800

    添加make-list-unknown-command配置项以伪装成命令不存在，并调整日志输出

--- src/main/java/me/dreamvoid/commandpermissionmanager/main.java
+++ src/main/java/me/dreamvoid/commandpermissionmanager/main.java
@@ -4,19 +4,25 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.Set;
 
 public class main extends JavaPlugin {
+    private static final ArrayList<String> commands = new ArrayList<>();
+
     @Override
     public void onLoad() {
         saveDefaultConfig();
-        if(getConfig().getBoolean("bStats",true)){
-            new Metrics(this, 15686);
-        }
     }
 
     @Override
@@ -25,50 +31,72 @@ public class main extends JavaPlugin {
         new BukkitRunnable() {
             @Override
             public void run() {
-                for(String s : getConfig().getStringList("commands")){
-                    String[] c = s.split(":");
-                    if(c.length != 2) {
-                        getLogger().warning("Format error found in config: " + s);
-                        continue;
-                    }
-                    try {
-                        Objects.requireNonNull(Bukkit.getPluginCommand(c[0])).setPermission(c[1]);
-                        getLogger().info("Set command " + c[0] + "'s permission to " + c[1]);
-                    } catch (NullPointerException e){
-                        getLogger().warning("Command \""+c[0]+"\" doesn't exist!");
-                    }
-                }
-
-                for(String s : getConfig().getStringList("commands-with-plugin")){
-                    String[] c = s.split(":");
-                    if(c.length != 2) {
-                        getLogger().warning("Format error found in config: " + s);
-                        continue;
-                    }
-                    try {
-                        Plugin p = Objects.requireNonNull(Bukkit.getPluginCommand(c[0])).getPlugin();
-
-                        for(String k : p.getDescription().getCommands().keySet()){
-                            Bukkit.getPluginCommand(k).setPermission(c[1]);
-                            getLogger().info("Set command " + k + "'s permission to " + c[1]);
-                        }
-                        getLogger().info("Set all of " + p.getName() + "'s commands permission to " + c[1]);
-                    } catch (NullPointerException e){
-                        getLogger().warning("Command or plugin \""+c[0]+"\" doesn't exist!");
-                    }
-                }
+                initial();
             }
         }.runTask(this);
+        if(getConfig().getBoolean("bStats",true)) new Metrics(this, 15686);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length > 0){
             if(args[0].equalsIgnoreCase("reload")){
-                onEnable();
+                reloadConfig();
+                initial();
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.reload","&eConfig reload, but it recommended to restart server if you delete some commands.")));
             } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.help","&eTry /commandpermissionmanager reload")));
         } else sender.sendMessage("This server is running "+ getDescription().getName() +" version "+ getDescription().getVersion()+" by "+ getDescription().getAuthors().toString().replace("[","").replace("]",""));
         return true;
+    }
+
+    private void initial(){
+        for(String s : getConfig().getStringList("commands")){
+            String[] c = s.split(":");
+            if(c.length != 2) {
+                getLogger().warning("Format error found in config: " + s);
+                continue;
+            }
+            try {
+                PluginCommand cmd = Bukkit.getPluginCommand(c[0]);
+                if(cmd.getPermission() != null || !Objects.equals(cmd.getPermission(), "")){
+                    getLogger().warning("It is recommend to use command " + c[0] + " own permission " + cmd.getPermission());
+                }
+                cmd.setPermission(c[1]);
+                if(getConfig().getBoolean("make-list-unknown-command", true)) cmd.setPermissionMessage("Unknown command. Type \"/help\" for help.");
+                commands.add(c[0]);
+                getLogger().info(String.format("Set permission of command \"%s\" to \"%s\"", c[0], c[1]));
+            } catch (NullPointerException e){
+                getLogger().warning("Command \""+c[0]+"\" doesn't exist!");
+            }
+        }
+
+        for(String s : getConfig().getStringList("commands-with-plugin")){
+            String[] c = s.split(":");
+            if(c.length != 2) {
+                getLogger().warning("Format error found in config: " + s);
+                continue;
+            }
+            try {
+                Plugin p = Bukkit.getPluginCommand(c[0]).getPlugin();
+                Set<String> keySet = p.getDescription().getCommands().keySet();
+                for (String k : keySet) {
+                    if(!commands.contains(k)) {
+                        PluginCommand cmd = Bukkit.getPluginCommand(k);
+                        if(cmd.getPermission() != null || !Objects.equals(cmd.getPermission(), "")){
+                            getLogger().warning("It is recommend to use command " + c[0] + " own permission " + cmd.getPermission());
+                        }
+                        cmd.setPermission(c[1]);
+                        if(getConfig().getBoolean("make-list-unknown-command", true)) cmd.setPermissionMessage("Unknown command. Type \"/help\" for help.");
+                    } else {
+                        getLogger().info("Skip changed command " + k);
+                    }
+                }
+                getLogger().info("Set permissions for "+ keySet.size() +" commands of plugin \"" + p.getName() + "\" to \"" + c[1] + "\"");
+
+            } catch (NullPointerException e){
+                getLogger().warning("Command or plugin \""+c[0]+"\" doesn't exist!");
+            }
+        }
+        commands.clear();
     }
 }
